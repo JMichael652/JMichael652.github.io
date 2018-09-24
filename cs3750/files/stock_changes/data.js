9ee267c0566013d6b6a070c068531f3c45361066
Joshua Redding <jmichael652@gmail.com>
2018-09-24 18:49:57 -0400

    Removed old CS 3750 assignment

--- cs3750/files/stock_changes/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o,p,q,r,s,t,u,v,w],x,_(y,z,A,B,C,D,E,_(),F,_(G,H,I,J,K,_(L,M,N,O),P,null,Q,J,R,J,S,T,U,null,V,W,X,Y,Z,ba,bb,W),bc,_(),bd,_(),be,_(bf,[])),bg,_(),bh,_());}; 
-var b="url",c="stock_changes.html",d="generationDate",e=new Date(1469733908896.64),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="itemtotal",l="hovername",m="keysPressed",n="L",o="R",p="Rhelp",q="Lhelp",r="gProductName",s="gProductPrice",t="PanelState",u="var1",v="var2",w="ScrollX",x="page",y="packageId",z="e4bf1512f5894ddd86385ee92df92ca3",A="type",B="Axure:Page",C="name",D="Stock Changes",E="notes",F="style",G="baseStyle",H="627587b6038d43cca051c114ac41ad32",I="pageAlignment",J="near",K="fill",L="fillType",M="solid",N="color",O=0xFFFFFFFF,P="image",Q="imageHorizontalAlignment",R="imageVerticalAlignment",S="imageRepeat",T="auto",U="favicon",V="sketchFactor",W="0",X="colorStyle",Y="appliedColor",Z="fontName",ba="Applied Font",bb="borderWidth",bc="adaptiveStyles",bd="interactionMap",be="diagram",bf="objects",bg="masters",bh="objectPaths";
-return _creator();
-})());
